0e216c758ea8b628d84a3ce45b6c8a6803e79e38
Pramallika <pramallikachunduru@gmail.com>
2025-07-09 22:17:56 +0100

    Delete tests directory

--- tests/homepageNavigation.spec.ts
+++
@@ -1,9 +0,0 @@
-
-import { test } from '@playwright/test';
-import { HomePage } from '../pages/HomePage';
-
-test('Homepage loads and displays the welcome heading', async ({ page }) => {
-  const homePage = new HomePage(page);
-  await homePage.goto();
-  await homePage.verifyWelcomeHeading('Welcome to Shady Meadows B&B');
-});
